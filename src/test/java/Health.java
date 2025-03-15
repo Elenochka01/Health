948c16e2fe1b30c5685ee7a22ea46531490fb7cd
Elenocka01 <oea1604@gmail.com>
2025-03-15 19:28:05 -0400

    Locators were added

--- src/test/java/Health.java
+++ src/test/java/Health.java
@@ -1,7 +1,6 @@
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -17,64 +16,64 @@ public class Health {
         System.out.println("Opened Website");
 
         Thread.sleep(3000);
-        WebElement about = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/ul[1]/li[2]/a[1]/div[1]/div[1]/p[1]"));
+        WebElement about = driver.findElement(By.xpath(Locators.about));
         Actions actions = new Actions(driver);
         actions.moveToElement(about).perform();
         System.out.println("Navigated About section");
 
         Thread.sleep(3000);
         WebDriverWait waitForWho = new WebDriverWait(driver, 25);
-        WebElement whoWeAre = waitForWho.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/div[1]/ul[1]/li[1]/a[1]/div[1]/div[1]/p[1]")));
+        WebElement whoWeAre = waitForWho.until(ExpectedConditions.elementToBeClickable(By.xpath(Locators.whoWeAre)));
         whoWeAre.click();
         System.out.println("Opened Who we Are");
 
         Thread.sleep(3000);
-        about = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/ul[1]/li[2]/a[1]/div[1]/div[1]/p[1]"));
+        about = driver.findElement(By.xpath(Locators.about));
         actions.moveToElement(about).perform();
 
         Thread.sleep(3000);
         WebDriverWait waitForWhat = new WebDriverWait(driver, 25);
-        WebElement whatWeDo = waitForWhat.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/div[1]/ul[1]/li[2]/a[1]/div[1]/div[1]/p[1]")));
+        WebElement whatWeDo = waitForWhat.until(ExpectedConditions.elementToBeClickable(By.xpath(Locators.whatWeDo)));
         whatWeDo.click();
         System.out.println("Opened What we do");
 
         Thread.sleep(3000);
-        about = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/ul[1]/li[2]/a[1]/div[1]/div[1]/p[1]"));
+        about = driver.findElement(By.xpath(Locators.about));
         actions.moveToElement(about).perform();
 
         Thread.sleep(3000);
         WebDriverWait waitForCulture = new WebDriverWait(driver, 25);
-        WebElement ourCulture = waitForCulture.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/div[1]/ul[1]/li[3]/a[1]/div[1]/div[1]/p[1]")));
+        WebElement ourCulture = waitForCulture.until(ExpectedConditions.elementToBeClickable(By.xpath(Locators.ourCulture)));
         ourCulture.click();
         System.out.println("Opened Our Culture");
 
         Thread.sleep(3000);
-        about = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/ul[1]/li[2]/a[1]/div[1]/div[1]/p[1]"));
+        about = driver.findElement(By.xpath(Locators.about));
         actions.moveToElement(about).perform();
 
         Thread.sleep(3000);
         WebDriverWait waitForImpact = new WebDriverWait(driver, 25);
-        WebElement ourImpact = waitForImpact.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/div[1]/ul[1]/li[4]/a[1]/div[1]/div[1]/p[1]")));
+        WebElement ourImpact = waitForImpact.until(ExpectedConditions.elementToBeClickable(By.xpath(Locators.ourImpact)));
         ourImpact.click();
         System.out.println("Opened Our Impact");
 
         Thread.sleep(3000);
-        about = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/ul[1]/li[2]/a[1]/div[1]/div[1]/p[1]"));
+        about = driver.findElement(By.xpath(Locators.about));
         actions.moveToElement(about).perform();
 
         Thread.sleep(3000);
         WebDriverWait waitForLeader = new WebDriverWait(driver, 25);
-        WebElement ourLeadership = waitForLeader.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/div[1]/ul[1]/li[5]/a[1]/div[1]/div[1]/p[1]")));
+        WebElement ourLeadership = waitForLeader.until(ExpectedConditions.elementToBeClickable(By.xpath(Locators.ourLeadership)));
         ourLeadership.click();
         System.out.println("Opened Leadership");
 
         Thread.sleep(3000);
-        WebElement button= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/footer[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[1]/div[3]/button[1]"));
+        WebElement button= driver.findElement(By.xpath(Locators.button));
         Actions actions1 = new Actions(driver);
         actions1.moveToElement(button).perform();
 
         Thread.sleep(3000);
-        WebElement email= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/footer[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[1]/div[2]/div[1]/input[1]"));
+        WebElement email= driver.findElement(By.xpath(Locators.email));
         email.sendKeys("123456@gmail.com");
 
         Thread.sleep(3000);
---
+++ src/test/java/Locators.java
@@ -0,0 +1,13 @@
+public class Locators {
+    public static final String about = "/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/ul[1]/li[2]/a[1]/div[1]/div[1]/p[1]";
+    public static final String whoWeAre = "/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/div[1]/ul[1]/li[1]/a[1]/div[1]/div[1]/p[1]";
+    public static final String whatWeDo = "/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/div[1]/ul[1]/li[2]/a[1]/div[1]/div[1]/p[1]";
+    public static final String ourCulture = "/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/div[1]/ul[1]/li[3]/a[1]/div[1]/div[1]/p[1]";
+    public static final String ourImpact = "/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/div[1]/ul[1]/li[4]/a[1]/div[1]/div[1]/p[1]";
+    public static final String ourLeadership = "/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/header[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/wix-dropdown-menu[1]/nav[1]/div[1]/ul[1]/li[5]/a[1]/div[1]/div[1]/p[1]";
+    public static final String button = "/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/footer[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[1]/div[3]/button[1]";
+    public static final String email = "/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/footer[1]/div[2]/div[2]/div[1]/div[1]/section[1]/div[2]/div[2]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[1]/div[2]/div[1]/input[1]";
+}
+
+
+
